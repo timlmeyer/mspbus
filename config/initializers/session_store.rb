4849a09361dd9ff2a4309d066dd6c7bb00e42187
Richard Barnes <rbarnes@umn.edu>
2013-06-01 14:59:54 -0500

    Changed variable definition format for backwards compatibility

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-MspBus::Application.config.session_store :cookie_store, key: '_msp_bus_session'
+MspBus::Application.config.session_store :cookie_store, :key => '_msp_bus_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information

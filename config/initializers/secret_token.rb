b0abf135edf3d4cee73cd1a3333370da0480da0e
blackjk3 <blackjk@gmail.com>
2013-06-01 13:00:24 -0500

    Initial commit of code.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MspBus::Application.config.secret_token = '284d09107bdecc98aafae8d5e7f7968869cafbb7cfddf4cacb27e53a9bb5444aa10b817fab92f9f2e3d6e0c825c007b8b10394f5fea9b1e68bdac4dfc568e0ed'

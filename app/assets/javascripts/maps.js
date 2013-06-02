8f758908e691863f5fb924e528228e74bfc2735a
blackjk3 <blackjk@gmail.com>
2013-06-02 12:56:15 -0500

    Added pins.

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,13 +1,28 @@
+var map;
+
 function initialize(lat,lon) {
   if (initialize.ran==true)
     return;
 
   var mapOptions = {
     center: new google.maps.LatLng(lat, lon),
-    zoom: 15,
+    zoom: 16,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
-  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 }
 
 //  google.maps.event.addDomListener(window, 'load', initialize);
+function add_markers(markers) {
+  if (initialize.ran==true)
+    return;
+
+  _.each(markers, function(item, index) {
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(item[1], item[0]),
+      map: map,
+      draggable: false,
+      animation: google.maps.Animation.DROP
+    });
+  });
+}

011a8dedb7775e73e179401f86e4fe09d62fb018
Richard Barnes <rbarnes@umn.edu>
2013-06-02 13:57:36 -0500

    Shortened description text in stops

--- app/assets/javascripts/show_stop.js
+++ app/assets/javascripts/show_stop.js
@@ -28,7 +28,7 @@ var ShowStopModel = Backbone.Model.extend({
 });
 
 // Realtime Template
-var realtime_template = _.template('<table><% _.each(data, function(item) { %> <tr><td class="route"><%= item.Route %><%= item.Terminal %></td><td class="desc"><%= item.Description %></td><td class="time" style="color:<%= item.priority %>"><i><%= item.DepartureText %></i> </td></tr><% }); %></table>');
+var realtime_template = _.template('<table><% _.each(data, function(item) { %> <tr><td class="route"><%= item.Route %><%= item.Terminal %></td><td><span class="desc" title="<%= item.Description %>"><%= item.sdesc %></span></td><td class="time" style="color:<%= item.priority %>"><i><%= item.DepartureText %></i> </td></tr><% }); %></table>');
 
 $(document).ready(function() {
   var id=$("#thestop").data().attr;
@@ -69,6 +69,10 @@ $(document).ready(function() {
 
         if(dtime<20 && obj.DepartureText.indexOf(":")!=-1)
           obj.DepartureText+='&nbsp;<i title="Bus scheduled, no real-time data available." class="icon-question-sign"></i>';
+
+        obj.sdesc=obj.Description;
+        if(obj.sdesc.length>25)
+          obj.sdesc=obj.Description.substr(0,25)+"&hellip;";
 
         console.log(obj.priority);
 

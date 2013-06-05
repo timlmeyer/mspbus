6c569ae7e7a7e6efb4a5b1afa9b50792f74f9f95
blackjk3 <blackjk@gmail.com>
2013-06-05 13:26:19 -0500

    Updated capistrano config to precompile assets.  Also it should automatically symlink database.yml to the shared folder.

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,8 @@ role :app, "debian2.brobston.com"
 role :db,  "debian2.brobston.com", :primary => true
 
 # if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+after 'deploy:update_code', 'deploy:symlink_db'
+after "deploy:restart", "deploy:cleanup"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
@@ -19,6 +20,10 @@ role :db,  "debian2.brobston.com", :primary => true
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
+  desc "Symlinks the database.yml"
+  task :symlink_db, :roles => :app do
+    run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+  end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end

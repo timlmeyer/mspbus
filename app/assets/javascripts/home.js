276c7f2519d998d926f11c18f114a90abd0e19fd
Richard Barnes <rbarnes@umn.edu>
2013-06-05 16:35:17 -0500

    Made screen layout change based on width of screen

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,21 @@
+$(window).resize(function() {
+  google.maps.event.trigger(map, "resize");
+  if ( matchMedia('only screen and (max-width: 767px)').matches ){ //Small Screen
+    $('#view-map').hide();
+    $('#view-table').removeClass('span6');
+    $('#view-table').addClass('span12');
+    $('#view-map').removeClass('span6');
+    $('#view-map').addClass('span12');
+  } else {
+    $('#view-map').show();
+    $('#view-table').show();
+    $('#view-table').removeClass('span12');
+    $('#view-table').addClass('span6');
+    $('#view-map').removeClass('span12');
+    $('#view-map').addClass('span6');
+  }
+});
+
 $(document).ready(function() {
   if( $('#view-map').css('display') !== 'none' ) {
     initialize(center.lat, center.lon);

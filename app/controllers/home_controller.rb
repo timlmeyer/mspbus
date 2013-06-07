65ce1850a33ec7da3c95ff554d0e496d7031a622
Richard Barnes <rbarnes@umn.edu>
2013-06-07 15:23:45 -0500

    Bound geolocations to the Twin Cities area. Provide message if this fails

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,6 +9,13 @@ class HomeController < ApplicationController
     end
     params[:radius] = 1
 
+    @inbounds=true
+    if not (44.47<=params[:lat].to_f and params[:lat].to_f<=45.42 and -94.01<=params[:lon].to_f and params[:lon].to_f<=-92.73)
+      params[:lat] = 44.979971
+      params[:lon] = -93.269797
+      @inbounds=false
+    end
+
     if !params[:q].present? && cookies[:q].present?
       params[:q]=cookies[:q]
     end

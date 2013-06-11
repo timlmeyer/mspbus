112fbb15105782d12fc58822ed62e0bbfe719226
blackjk3 <blackjk@gmail.com>
2013-06-11 13:55:26 -0500

    Trying chgrp as sudo.

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ after 'deploy:update_code', 'deploy:symlink_db', :setup_group
 after "deploy:restart", "deploy:cleanup"
 
 task :setup_group do
-  run "chgrp mspbus #{deploy_to} -R"
+  run "#{sudo} chgrp mspbus #{deploy_to} -R"
 end
 
 # if you're still using the script/reaper helper you will need

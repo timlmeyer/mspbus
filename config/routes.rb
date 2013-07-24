0dd3a22a24e78884da6f220c9be1f881bad85b6d
Richard Barnes <rbarnes@umn.edu>
2013-07-23 22:18:15 -0400

    Added table view

--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,7 @@ MspBus::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'home#index'
+  get '/table' => 'home#table'
   get 'stop/bounds' => 'stop#bounds'
   get 'stop/closest_trip' => 'stop#closest_trip'
   get 'stop/:id' => 'stop#show'

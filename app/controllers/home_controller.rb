769aacef5c5265a50abcf918454fbc3595c776c0
Richard Barnes <rbarnes@umn.edu>
2013-07-24 22:54:55 -0400

    Added text message listings of bus arrival times

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -50,8 +50,44 @@ class HomeController < ApplicationController
   end
 
   def sms
+    #  Twilio POST request parameters
+    #  Parameter	Description
+    #  SmsSid	A 34 character unique identifier for the message. May be used to later retrieve this message from the REST API.
+    #  AccountSid	The 34 character id of the Account this message is associated with.
+    #  From	The phone number that sent this message.
+    #  To	The phone number of the recipient.
+    #  Body	The text body of the SMS message. Up to 160 characters long.
+    if not params[:Body].index(' ')
+      @stopid=params[:Body]
+      @stops=Stop.get_stop_by_id({:id=>@stopid})
+      if @stops.results.empty?
+        @stopfound=false
+        return
+      end
 
+      response = HTTParty.get("http://svc.metrotransit.org/NexTrip/#{@stopid}?format=json")
+      puts "http://svc.metrotransit.org/NexTrip/#{@stopid}?format=json"
+      if not response.code==200
+        @error=true
+        return
+      end
+      @stopfound=true
+      puts response.body, response.code, response.message, response.headers.inspect
+      arrivals = JSON.parse response.body
 
+      @smess=""
+      response.each do |item|
+        @smess+=item['RouteDirection'][0]+item['Route']+item['Terminal']+" "+item['DepartureText']+", "
+      end
+      @smess=@smess[0..159]
+      if @smess[-2]==','
+        @smess=@smess[0..-3]
+      end
+    else
+      puts "Space"
+    end
+
+    render :layout => false
   end
 
   def about

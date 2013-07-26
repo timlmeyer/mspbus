10d44d372521ce75e47b75a1031848c4ad9c4476
Richard Barnes <rbarnes@umn.edu>
2013-07-26 10:18:59 -0400

    Added click and keypress events for searchbox

--- app/assets/javascripts/realtime.js
+++ app/assets/javascripts/realtime.js
@@ -107,4 +107,6 @@ $(document).ready(function() {
   window.setInterval(update_table, 60000);
 
   $('.btn-current-location').on('click', update_coordinates);
+  $("#q").on("keypress", function(e) { if (e.which == 13) geocode($("#q").val()); });
+  $("#qsub").click(function() { geocode($("#q").val()); });
 });

a40b7fc8fe409897bb67ae8b091318d88a7ecf85
Richard Barnes <rbarnes@umn.edu>
2013-07-29 09:30:06 -0500

    Improved spacing of code

--- app/controllers/stop_controller.rb
+++ app/controllers/stop_controller.rb
@@ -2,6 +2,7 @@ class StopController < ApplicationController
   def show
     @stop = Stop.get_stop_by_id({:id => params[:id] }).results.first
   end
+
   def bounds
     @stop = Stop.get_stop_by_bounds(params[:n], params[:s], params[:e], params[:w])
     @stop = @stop.map{ |i| {:lon=>i['location'][0],:lat=>i['location'][1],:id=>i['stop_id'].to_i, :name => i['stop_name'] } }
@@ -10,6 +11,7 @@ class StopController < ApplicationController
       format.json { render :json => @stop }
     end
   end
+
   def closest_trip
     
     # shape_array = []

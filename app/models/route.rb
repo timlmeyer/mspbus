ec3935f4dbebeb217aa47584f4973dc15a86a541
Richard Barnes <rbarnes@umn.edu>
2013-07-29 00:19:21 -0500

    Narrowed route results to a single trip

--- app/models/route.rb
+++ app/models/route.rb
@@ -2,12 +2,25 @@ class Route < ActiveRecord::Base
 
   self.table_name = 'flat_routes'
 
-  def self.get_stop_list(route_id)
+  def self.get_trip_beginning_now(route_id)
     today = Date.today.strftime("%Y%m%d")
-    select('stop_id, stop_name, stop_sequence, stop_lat, stop_lon')
-    .where("start_date<='#{today}' AND '#{today}'<=end_date AND #{Date.today.strftime("%A").downcase} = '1' AND route_id='#{route_id}-62'")
-    .order('stop_sequence')
+    timenow = Date.today.strftime("%H:%M:00")
+
+    #TODO: Need to account for the direction
+    select('trip_id')
+    .where("start_date<='#{today}' AND '#{today}'<=end_date AND #{Date.today.strftime("%A").downcase} = '1' AND route_id='#{route_id}-62' AND arrival_time>='#{timenow}'")
+    .order('arrival_time')
+    .limit(1)
   end
 
+  #TODO: Need to account for the direction
+  def self.get_stop_list(route_id)
+    today = Date.today.strftime("%Y%m%d")
+    trip_id=get_trip_beginning_now(route_id).first().trip_id
+
+    select('stop_id, stop_name, stop_sequence, stop_lat, stop_lon')
+    .where("trip_id='#{trip_id}'")
+    .order('stop_sequence')
+  end
 
 end

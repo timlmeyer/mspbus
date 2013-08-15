55d9b7e96ec037826a6ee4a227c99de2f27469f2
Coffeeman <louis@goessling.com>
2013-08-15 14:32:18 -0500

    Added keys to config files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,6 @@ MspBus::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  config.google_maps_api_key="AIzaSyBz7GjhrxP5ntdxLX79fi6LaLdn9Lc6LFY"
 end
